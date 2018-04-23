7beb4f55b6dd1927541d1ca890ac003b4cd87f1f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dennis.ImportData/Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace Dennis.ImportData {
+    public sealed partial class ImportDataModule : ModuleBase {
+        public ImportDataModule() {
+            InitializeComponent();
+        }
+    }
+}
